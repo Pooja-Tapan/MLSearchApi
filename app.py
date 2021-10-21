f6fdf61311a0e5ba64171deef3d2189d9aedd2c3
Pooja-Tapan <84371595+Pooja-Tapan@users.noreply.github.com>
2021-10-21 17:33:34 +0530

    new changes

---
+++ app.py
@@ -0,0 +1,90 @@
+# import required packages
+from flask import Flask, request, render_template
+from matches import *
+
+import numpy as np
+from PIL import Image
+from io import BytesIO
+import json
+
+from azure.core.exceptions import ResourceNotFoundError
+from azure.storage.fileshare import ShareFileClient
+
+
+# define azure file download function
+def download_azure_file(connection_string, share_name, library_id, file_name, reference_images_pickle):
+    try:
+        # Build the remote path
+        source_file_path = library_id + "/" + file_name
+
+        # Create a ShareFileClient from a connection string
+        file_client = ShareFileClient.from_connection_string(connection_string, share_name, source_file_path)
+
+        print("Downloading to:", reference_images_pickle)
+
+        # Open a file for writing bytes on the local system
+        with open(reference_images_pickle, "wb") as data:
+            # Download the file from Azure into a stream
+            stream = file_client.download_file()
+            # Write the stream to the local file
+            data.write(stream.readall())
+
+    except ResourceNotFoundError as ex:
+        print("ResourceNotFoundError:", ex.message)
+
+
+# initialize connection string and other parameters to download reference pickle file
+connection_string = "DefaultEndpointsProtocol=https;AccountName=mvlabs;AccountKey=FWWRSow1FFrskddjEmPnl70PcSk7F2Os5thkRninwcstW3AqgZfPxGlp/QHqCzJNRFOHNhBFK9YfkJCCm57WSA==;EndpointSuffix=core.windows.net"
+share_name = "indexstore"
+file_name = "rn50.pkl"
+
+# define Flask API and return matching images
+app = Flask(__name__)
+
+
+@app.route('/<library_id>', methods=['GET', 'POST', 'PATCH'])
+def hello_world(library_id):
+    # check if reference pickle file already exists for the mentioned library_id
+    # If not, download the pickle file for the first time
+    reference_images_dir = "reference_pickle" + "/" + library_id
+    os.makedirs(reference_images_dir, exist_ok=True)
+
+    reference_images_pickle = reference_images_dir + "/" + file_name
+
+    if not (os.path.exists(reference_images_pickle)):
+        print("Downloading Reference Pickle File For the First Time")
+        download_azure_file(connection_string, share_name, library_id, file_name, reference_images_pickle)
+    else:
+        print("Reference Pickle File Already Exists")
+
+    if request.method == 'GET':
+        return render_template('index.html', value='hi')
+
+    if request.method == 'POST':
+        print(request.files)
+        if 'file' not in request.files:
+            print('file not uploaded')
+            return
+    
+    # method for removing old pickle and downloading new pickle file
+    if request.method == 'PATCH':
+        os.remove(reference_images_pickle)
+        
+        patch_status = {"Flag" : "Failed"}        
+        try:    
+            download_azure_file(connection_string, share_name, library_id, file_name, reference_images_pickle)
+            patch_status["Flag"] = "Success"
+        except:
+            print("Unable to download new pickle file")
+        
+        return patch_status        
+
+    file = request.files['file'].read()
+    output_json = get_result(file, reference_images_pickle)
+
+    return json.dumps(output_json)
+
+
+#if __name__ == '__main__':
+    #app.run(debug=True)
+    #app.run(host="0.0.0.0", port=5000)
---
+++ matches.py
@@ -0,0 +1,119 @@
+# import required packages
+
+import os
+from torchvision import transforms, datasets
+# import pandas as pd
+import torch
+import torch.nn as nn
+from PIL import Image
+import io
+from torch.utils.data import Dataset
+from torchvision import models, transforms
+from tqdm import tqdm
+import numpy as np
+import pickle
+# import base64
+import glob
+import sys
+
+
+# Define MediaValet Dataset Class
+class MediaValetDataset(Dataset):
+
+    def __init__(self, image_bytes, transform):
+        self.transform = transform
+        self.files = [image_bytes]
+
+    def __len__(self):
+        return len(self.files)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        image_bytes_array = self.files[idx]
+        image = Image.open(io.BytesIO(image_bytes_array)).convert("RGB")
+
+        return self.transform(image)
+
+
+# Define Macros
+num_matches = 20
+DEVICE = torch.device("cpu:0")
+
+# Load resnet pretrained model
+model = models.resnet50(pretrained=True)
+model.eval()
+model = model.to(DEVICE)
+cut_model = nn.Sequential(*list(model.children())[:-1])
+
+
+# Define Get Matches Function
+def get_matches(pair_dists, selected_indicies, num_matches=10):
+    k = min(num_matches, selected_indicies.shape[0])
+    dists, inds = torch.topk(pair_dists[selected_indicies, :], k, dim=0, sorted=True)
+    return (selected_indicies[inds].detach().cpu().numpy(), dists.detach().cpu().numpy())
+
+
+# Get Final Result in json format
+def get_result(test_image_bytes, reference_images_pickle):
+    data_transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+
+    # Use Test image path
+    dataset = MediaValetDataset(test_image_bytes, data_transform)
+    data_loader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=False, num_workers=0)
+
+    # Generate features and tensor for test image
+    for i, (inputs) in enumerate(tqdm(data_loader)):
+        inputs = inputs.to(DEVICE)
+        test_image_feature = torch.squeeze(cut_model(inputs)).detach().cpu().numpy()
+        test_image_feature = np.reshape(test_image_feature, (1, test_image_feature.size))
+
+    test_image_tensor = torch.from_numpy(test_image_feature).float().to("cpu:0")
+    test_image_tensor = test_image_tensor / torch.sqrt(torch.sum(test_image_tensor ** 2, dim=1, keepdim=True))
+    test_image_tensor = test_image_tensor.to(DEVICE)
+
+    # Read pickle file and features of database images to get matching images
+    with open(reference_images_pickle, "rb") as f:
+        (reference_images_features, reference_images_attributes) = pickle.load(f)
+
+    # print(reference_images_features.shape)
+    # print(reference_images_attributes)
+
+    reference_images_tensor = torch.from_numpy(reference_images_features).float().to("cpu:0")
+    reference_images_tensor = reference_images_tensor / torch.sqrt(
+        torch.sum(reference_images_tensor ** 2, dim=1, keepdim=True))
+    reference_images_tensor = reference_images_tensor.to(DEVICE)
+
+    indicies = torch.arange(0, reference_images_tensor.shape[0]).to(DEVICE)
+
+    # Get top-k matching images in JSON format
+    pair_dists = torch.einsum("nf,bf->nb", reference_images_tensor, test_image_tensor)
+    matched_indicies, matched_scores = get_matches(pair_dists, indicies, num_matches=num_matches)
+
+    dict_match = []
+    for i in range(len(matched_indicies)):
+        filename = reference_images_attributes["orig_name"][matched_indicies[i][0]].split("/")[-1]
+        sim_score = round(matched_scores[i][0], 2)
+        
+        # condition for duplicate images       
+        if sim_score>=0.99:        
+            image_type="duplicate"
+        # condition for derivative images
+        elif sim_score>=0.97:
+            image_type="derivative"     
+        else:
+            image_type=""
+            
+        if sim_score<0.6:
+            pass 
+        else:
+            dict_ind = {"filename": str(filename), "score": str(sim_score), "type": str(image_type)}
+            dict_match.append(dict_ind)
+            
+    return dict_match
+
