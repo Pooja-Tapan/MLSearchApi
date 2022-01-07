bd2a6502fd495ddd9540485b9c19eaca174d63f4
Pooja-Tapan <84371595+Pooja-Tapan@users.noreply.github.com>
2022-01-07 16:50:28 +0530

    modified for performance

--- app.py
+++ app.py
@@ -10,36 +10,11 @@ import json
 from azure.core.exceptions import ResourceNotFoundError
 from azure.storage.fileshare import ShareFileClient
 
-# define azure file download function
-def download_azure_file(connection_string, share_name, library_id, file_name, reference_images_pickle):
-    try:
-        # Build the remote path
-        source_file_path = library_id + "/" + file_name
-
-        # Create a ShareFileClient from a connection string
-        file_client = ShareFileClient.from_connection_string(connection_string, share_name, source_file_path)
-
-        print("Downloading to:", reference_images_pickle)
-
-        # Open a file for writing bytes on the local system
-        with open(reference_images_pickle, "wb") as data:
-            # Download the file from Azure into a stream
-            stream = file_client.download_file()
-            # Write the stream to the local file
-            data.write(stream.readall())
-
-    except ResourceNotFoundError as ex:
-        print("ResourceNotFoundError:", ex.message)
-
-
 # initialize connection string and other parameters to download reference pickle file
-connection_string = "DefaultEndpointsProtocol=https;AccountName=mvlabs;AccountKey=FWWRSow1FFrskddjEmPnl70PcSk7F2Os5thkRninwcstW3AqgZfPxGlp/QHqCzJNRFOHNhBFK9YfkJCCm57WSA==;EndpointSuffix=core.windows.net"
-share_name = "indexstore"
 file_name = "rn50.pkl"
 
 # define Flask API and return matching images
 app = Flask(__name__)
-
 
 @app.route('/<library_id>', methods=['GET', 'POST'])
 def hello_world(library_id):
@@ -49,20 +24,12 @@ def hello_world(library_id):
     # os.makedirs(reference_images_dir, exist_ok=True)
 
     reference_images_pickle = reference_images_dir + "/" + file_name
-
-    # if not (os.path.exists(reference_images_pickle)):
-        # print("Downloading Reference Pickle File For the First Time")
-        # download_azure_file(connection_string, share_name, library_id, file_name, reference_images_pickle)
-    # else:
-        # print("Reference Pickle File Already Exists")
-
+   
     if request.method == 'GET':
         return render_template('index.html', value='hi')
 
     if request.method == 'POST':
-        print(request.files)
         if 'file' not in request.files:
-            print('file not uploaded')
             return
         
     file = request.files['file'].read()
